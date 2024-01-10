442267fa9d6964316f5326538253b3d6f1009310
Simioyin222 <simi.oyinkolade@gmail.com>
2024-01-10 01:29:30 -0600

    install mongose and its functionality

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
+
+mongoose.connect('your-mongodb-connection-string', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // connection
+  console.log("Connected to MongoDB");
+});
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
