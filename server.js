a81699dd24e7a6153a63c409b5f361ec526d222f
Simioyin222 <simi.oyinkolade@gmail.com>
2024-01-10 01:22:58 -0600

    fix sytax issue with console log for server

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port,() => {
-  console.log(server running on port ${port});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
