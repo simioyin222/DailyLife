65f496c3fc56cb7b4f886f7f03d6e58912251c95
Simioyin222 <simi.oyinkolade@gmail.com>
2024-01-13 00:34:02 -0600

    change database format of mongodb connection

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 const port = 3001;
 
 // Connect to MongoDB
-mongoose.connect(mongodb+srv://simioyin222:Godlove6@blog.zdeh0v9.mongodb.net/
-, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://simioyin222:Godlove6@blog.zdeh0v9.mongodb.net/BLOG', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
