4c3404f0766a6148c42aa55110cb591f01a0521d
Simioyin222 <simi.oyinkolade@gmail.com>
2024-01-13 00:43:32 -0600

    protecting key and information now

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const postRoutes = require('./routes/posts');
@@ -8,7 +9,7 @@ const app = express();
 const port = 3001;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://simioyin222:Godlove6@blog.zdeh0v9.mongodb.net/BLOG', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
